efe7978f7845354a3fe60e8cb78c032aebba8d92
sb8956 <65844764+sb8956@users.noreply.github.com>
2020-08-05 22:50:22 +0900

    Login Value

---
+++ app/src/main/java/com/example/appexample/LoginResultActivity.java
@@ -0,0 +1,33 @@
+package com.example.appexample;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+public class LoginResultActivity extends AppCompatActivity {
+
+    TextView TextView_get;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView_get=findViewById(R.id.TextView_get);
+
+        Intent intent = getIntent();
+        intent.getExtras();
+
+        Bundle bundle=intent.getExtras();
+        String email = bundle.getString("email");
+        String password = bundle.getString("password");
+
+        TextView_get.setText(email+" / "+password);
+    }
+}
--- app/src/main/java/com/example/appexample/MainActivity.java
+++ app/src/main/java/com/example/appexample/MainActivity.java
@@ -1,14 +1,46 @@
 package com.example.appexample;
 
-import androidx.appcompat.app.AppCompatActivity;
+        import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+        import android.content.Intent;
+        import android.os.Bundle;
+        import android.view.View;
+        import android.widget.LinearLayout;
+        import android.widget.RelativeLayout;
+
+        import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextInputEditText TextInputEditText_eamil, TextInputEditText_password;
+    RelativeLayout RelativeLayout_login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        TextInputEditText_eamil=findViewById(R.id.TextInputEditText_email);
+        TextInputEditText_password=findViewById(R.id.TextInputEditText_password);
+        RelativeLayout_login =findViewById(R.id.RelativeLayout_login);
+
+        //1. 값을 가져온다.
+        //2. 클릭을 감지한다.
+        //3. 1번 값을 다음 액티비티로 넘긴다.
+
+
+        RelativeLayout_login.setClickable(true);
+        RelativeLayout_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = TextInputEditText_eamil.getText().toString();
+                String password = TextInputEditText_password.getText().toString();
+
+                Intent intent = new Intent(MainActivity.this , LoginResultActivity.class);
+                intent.putExtra("email",email);
+                intent.putExtra("password", password);
+                startActivity(intent);
+            }
+        });
     }
 }
